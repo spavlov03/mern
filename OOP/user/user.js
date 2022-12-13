27a7426ec8f741fba4ceb54926980cc45cdd25e4
Svet Pavlov <pavlov.svetoslav@gmail.com>
2022-12-12 16:41:25 -0800

    User Chaining

--- OOP/user/user.js
+++ OOP/user/user.js
@@ -6,16 +6,20 @@ class User {
   }
   makeDeposit(amount) {
     this.accountBalance += amount; 
+    return this;
   }
   makeWithdrawal(amount) { 
     this.accountBalance -= amount;
+    return this;
   }
   displayBalance() { 
     console.log(`User: ${this.name}, Balance:$${this.accountBalance}`)
+    return this;
   }
   transferMoney(otherUser,amount) {
     this.accountBalance -= amount; 
     otherUser.accountBalance += amount;
+    return this;
   }
 }
 
@@ -23,21 +27,8 @@ const svet = new User("spavlov","spavlov@hotmail.com");
 const pavel = new User("pavel","pavel@gmail.com"); 
 const mitko = new User("mitko","mitkomb@gmail.com")
 
-svet.makeDeposit(100)
-svet.makeDeposit(50);
-svet.makeDeposit(25); 
-svet.makeWithdrawal(5); 
-svet.displayBalance();
-pavel.makeDeposit(100); 
-pavel.makeDeposit(50); 
-pavel.makeWithdrawal(25); 
-pavel.makeWithdrawal(5); 
-pavel.displayBalance();
-mitko.makeDeposit(200); 
-mitko.makeWithdrawal(25);
-mitko.makeWithdrawal(15);
-mitko.makeWithdrawal(10);
-mitko.displayBalance();
-svet.transferMoney(mitko,70); 
-svet.displayBalance();
+svet.makeDeposit(100).makeDeposit(50).makeDeposit(25).makeWithdrawal(5).displayBalance();
+pavel.makeDeposit(100).makeDeposit(50).makeWithdrawal(25).makeWithdrawal(5).displayBalance();
+mitko.makeDeposit(200).makeWithdrawal(25).makeWithdrawal(15).makeWithdrawal(10).displayBalance();
+svet.transferMoney(mitko,70).displayBalance();
 mitko.displayBalance();
